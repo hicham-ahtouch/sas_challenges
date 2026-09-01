f0353cd25a88d292904f8a81cb9fdfd6aa1b9fad
hicham ahtouch <ahtouchhicham2015@gmail.com>
2026-09-01 11:43:18 +0100

    let_update

--- day0/let.js
+++ day0/let.js
@@ -4,3 +4,11 @@ console.log(age);
 console.log(name);
 age = 25;
 console.log(name, "in 09/09/2026 will be", age ,"years old");
+//some math
+let a = 10;
+let b = 5;
+console.log(a + b);//result will be 15
+console.log(a - b);//result will be 5
+console.log(a * b);//result will be 50
+console.log(a / b);//result will be 2
+console.log(a % b);//result will be 0
