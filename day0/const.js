e2eb5b8d5b2b2428d4da9b4c677ba24e0f2a2a28
hicham ahtouch <ahtouchhicham2015@gmail.com>
2026-09-02 16:02:09 +0100

    challenge1

--- day0/const.js
+++ day0/const.js
@@ -2,4 +2,4 @@ let name = "hicham";
 let age = 24;
 const country = "Morocco";
 const sex = "male";
-console.log(name , "from", country , "is" , sex , "and he's" , age , "years old");
+console.log(name , "from", country , "is" , sex , "and he's " + age , "years old");
